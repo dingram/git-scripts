511465cd17d4cff88da3c8399b6ca1294c105ca5
Dave Ingram <dmi@teamgroupspaces.com>
2011-10-18 16:58:29 +0100

    Added simple abbreviations

---
+++ aliases/abbreviations.sh
@@ -0,0 +1,6 @@
+git config --global alias.ci commit
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.di diff
+git config --global alias.sta stash
+git config --global alias.st status
