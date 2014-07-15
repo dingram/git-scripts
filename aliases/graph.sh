e520d9d94c3c24ac4f9636706353e5132f080952
Dave Ingram <dave@dmi.me.uk>
2014-07-14 21:08:57 -0700

    Fix formatting output of "git lg" when used with -p

--- aliases/graph.sh
+++ aliases/graph.sh
@@ -1,4 +1,4 @@
 git config --global alias.graph log --decorate --oneline --graph --branches --date-order
 git config --global alias.graph-all log --decorate --oneline --graph --all --date-order
 git config --global alias.gr log --decorate --oneline --graph --date-order
-git config --global alias.lg log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative --date-order
+git config --global alias.lg log --graph --pretty=tformat:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative --date-order
